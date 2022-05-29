e6e7ee8fb243ff4918368f633d93da20056bad1b
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-05-29 16:54:07 +0800

    Added customer repo and moved out to trading packages

---
+++ preferences/src/main/java/com/yugabyte/samples/trading/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.yugabyte.samples.trading.repository;
+
+import com.yugabyte.samples.trading.model.Customer;
+import com.yugabyte.samples.trading.model.Preference;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "customers")
+@OpenAPIDefinition()
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+
+}
