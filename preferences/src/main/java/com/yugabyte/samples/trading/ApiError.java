4a33f032c63e5ee527cd63fa463abd54e61014b0
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-05-30 22:54:41 +0800

    Ignore missing fields

--- preferences/src/main/java/com/yugabyte/samples/trading/ApiError.java
+++ preferences/src/main/java/com/yugabyte/samples/trading/ApiError.java
@@ -1,19 +1,22 @@
 package com.yugabyte.samples.trading;
 
-import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import java.util.List;
 import lombok.Builder;
-import lombok.Value;
 
 
 @Builder
-public record ApiError (
+public record ApiError(
   String message,
+  @JsonInclude(Include.NON_NULL)
   List<FieldError<?>> fieldErrors
-  ){
+) {
 
 }
 
 
 @Builder
-record FieldError<T>(String fieldName, T value, String error) {}
+record FieldError<T>(String fieldName, T value, String error) {
+
+}
