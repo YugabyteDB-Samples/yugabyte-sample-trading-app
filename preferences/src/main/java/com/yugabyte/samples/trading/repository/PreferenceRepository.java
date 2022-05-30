aa1fffb7662d82ca8c640ad30ae35d738ed0b191
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-05-30 22:48:02 +0800

    Added bearer token req for openapi

--- preferences/src/main/java/com/yugabyte/samples/trading/repository/PreferenceRepository.java
+++ preferences/src/main/java/com/yugabyte/samples/trading/repository/PreferenceRepository.java
@@ -2,11 +2,13 @@ package com.yugabyte.samples.trading.repository;
 
 import com.yugabyte.samples.trading.model.Preference;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "preferences")
 @OpenAPIDefinition()
+@SecurityRequirement(name = "auth-header-bearer")
 public interface PreferenceRepository extends JpaRepository<Preference, Integer> {
 
 
