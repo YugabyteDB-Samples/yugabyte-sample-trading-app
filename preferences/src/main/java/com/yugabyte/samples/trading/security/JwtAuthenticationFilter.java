46ea983b9bc19ad358d61ea31215a884ceb36875
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-05-30 22:49:47 +0800

    Cleanup

--- preferences/src/main/java/com/yugabyte/samples/trading/security/JwtAuthenticationFilter.java
+++ preferences/src/main/java/com/yugabyte/samples/trading/security/JwtAuthenticationFilter.java
@@ -5,7 +5,9 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.NonNullApi;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,8 +19,8 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-  private JwtCodec jwtCodec;
-  private UserDetailsManager users;
+  private final JwtCodec jwtCodec;
+  private final UserDetailsManager users;
 
   public JwtAuthenticationFilter(JwtCodec jwtCodec, UserDetailsManager users) {
     this.jwtCodec = jwtCodec;
@@ -26,7 +28,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
   }
 
   @Override
-  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+  protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
     try {
       String jwt = getJwtFromRequest(request);
 
@@ -50,7 +52,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
   private String getJwtFromRequest(HttpServletRequest request) {
     String bearerToken = request.getHeader("Authorization");
     if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-      return bearerToken.substring(7, bearerToken.length());
+      return bearerToken.substring(7);
     }
     return null;
   }
