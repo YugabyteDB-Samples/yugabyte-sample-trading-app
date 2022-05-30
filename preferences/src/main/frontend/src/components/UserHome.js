dac6bae9e2cf3a87894621e5453d1b62660fe8e1
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-05-30 18:13:30 +0800

    New user home page

---
+++ preferences/src/main/frontend/src/components/UserHome.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Chart from './Chart';
+
+export default function UserHome() {
+  return (
+    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+    <Grid container spacing={3}>
+      <Grid item  xs={12} md={12} lg={12}>
+        <Paper
+          sx={{
+            p: 2,
+            display: 'flex',
+            flexDirection: 'column',
+            height: 240
+          }}
+        >
+          <Chart />
+        </Paper>
+      </Grid>
+    </Grid>
+  </Container>
+  )
+};
+
