c92812e1064cddb2ce3f750eee1f5043a7277c86
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-05-30 18:14:39 +0800

    Added find by email

--- preferences/src/main/java/com/yugabyte/samples/trading/repository/CustomerRepository.java
+++ preferences/src/main/java/com/yugabyte/samples/trading/repository/CustomerRepository.java
@@ -3,12 +3,13 @@ package com.yugabyte.samples.trading.repository;
 import com.yugabyte.samples.trading.model.Customer;
 import com.yugabyte.samples.trading.model.Preference;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "customers")
 @OpenAPIDefinition()
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-
+  public Optional<Customer> findByContactEmail(String contactEmail);
 
 }
