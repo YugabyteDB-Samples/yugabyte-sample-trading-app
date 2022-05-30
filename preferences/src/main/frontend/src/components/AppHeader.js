46f9894d19db8308973a9e7b1be000393a14702f
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-05-30 18:12:57 +0800

    Minor change in image name

--- preferences/src/main/frontend/src/components/AppHeader.js
+++ preferences/src/main/frontend/src/components/AppHeader.js
@@ -139,7 +139,7 @@ const AppHeader = () => {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.png" />
               </IconButton>
             </Tooltip>
             <Menu
