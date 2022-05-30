602a504923ebfc7d28295572a4639279c6d33219
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-05-30 22:47:54 +0800

    Added bearer token req for openapi

--- preferences/src/main/java/com/yugabyte/samples/trading/repository/CustomerRepository.java
+++ preferences/src/main/java/com/yugabyte/samples/trading/repository/CustomerRepository.java
@@ -1,15 +1,17 @@
 package com.yugabyte.samples.trading.repository;
 
 import com.yugabyte.samples.trading.model.Customer;
-import com.yugabyte.samples.trading.model.Preference;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "customers")
 @OpenAPIDefinition()
+@SecurityRequirement(name = "auth-header-bearer")
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-  public Optional<Customer> findByContactEmail(String contactEmail);
+
+  Optional<Customer> findByContactEmail(String contactEmail);
 
 }
