9324593e12602a82b36336d5d207838222d80c66
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-05-30 18:11:59 +0800

    update password and add sessions schema

--- preferences/src/main/resources/db/migration/V1_0_3__auth_data.sql
+++ preferences/src/main/resources/db/migration/V1_0_3__auth_data.sql
@@ -1,12 +1,12 @@
 -- All Password = Password#123
 
 INSERT INTO users (username, password)
-VALUES ('superadmin', '{bcrypt}$2a$10$tSM9pJKQBwcTUmF2JU0OMeXp.xw5Jrpkod9We7ViCCAJhi16OBCBC'),
-       ('admin', '{bcrypt}$2a$10$H8HTaI2dT9Udr2OfsY8n3eZQX.dZTiSCJOAxD9zeRTX9yZ/OAIE2.'),
-       ('support', '{bcrypt}$2a$10$H8HTaI2dT9Udr2OfsY8n3eZQX.dZTiSCJOAxD9zeRTX9yZ/OAIE2.'),
-       ('test1@gmail.com', '{bcrypt}$2a$10$GddY6N91rEZb2w8HctJ22OpTew9rEbBOzvHmoNBRW0eBTns57cLPW'),
-       ('test2@gmail.com', '{bcrypt}$2a$10$yff4ZmyqBIOsYPBe.NDnte7DR7bTdT.4T/YlkeB02boc5HdH/hg1W'),
-       ('test3@gmail.com', '{bcrypt}$2a$10$v7K90bPUvAubpRMBcjEdQOaQR87LsGLN0kOCELk24GB3cMy7siWNi');
+VALUES ('superadmin', '$2a$10$tSM9pJKQBwcTUmF2JU0OMeXp.xw5Jrpkod9We7ViCCAJhi16OBCBC'),
+       ('admin', '$2a$10$H8HTaI2dT9Udr2OfsY8n3eZQX.dZTiSCJOAxD9zeRTX9yZ/OAIE2.'),
+       ('support', '$2a$10$H8HTaI2dT9Udr2OfsY8n3eZQX.dZTiSCJOAxD9zeRTX9yZ/OAIE2.'),
+       ('test1@gmail.com', '$2a$10$GddY6N91rEZb2w8HctJ22OpTew9rEbBOzvHmoNBRW0eBTns57cLPW'),
+       ('test2@gmail.com', '$2a$10$yff4ZmyqBIOsYPBe.NDnte7DR7bTdT.4T/YlkeB02boc5HdH/hg1W'),
+       ('test3@gmail.com', '$2a$10$v7K90bPUvAubpRMBcjEdQOaQR87LsGLN0kOCELk24GB3cMy7siWNi');
 
 INSERT INTO authorities (username, authority)
 VALUES ('superadmin', 'SUPERADMIN'),
