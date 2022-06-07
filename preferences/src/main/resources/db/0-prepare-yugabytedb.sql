01d418949a0407a97b94f14c56c4c8fdbc489e3f
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-06-07 22:06:44 +0800

    Fake change

--- preferences/src/main/resources/db/0-prepare-yugabytedb.sql
+++ preferences/src/main/resources/db/0-prepare-yugabytedb.sql
@@ -2,9 +2,11 @@
 CREATE TABLESPACE us_tablespace WITH (
   replica_placement='{ "num_replicas": 1, "placement_blocks":[{"cloud":"aws","region":"us-east-2","zone":"us-east-2c","min_num_replicas":1}]}'
   );
+
 CREATE TABLESPACE eu_tablespace WITH (
   replica_placement='{"num_replicas": 1, "placement_blocks":[{"cloud":"aws","region":"eu-central-1","zone":"eu-central-1c","min_num_replicas":1}]}'
   );
+
 CREATE TABLESPACE ap_tablespace WITH (
   replica_placement='{"num_replicas": 1, "placement_blocks":[{"cloud":"aws","region":"ap-southeast-1","zone":"ap-southeast-1c","min_num_replicas":1}]}'
   );
