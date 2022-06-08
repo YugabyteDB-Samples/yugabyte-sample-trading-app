5d24f2c441b58f786dd9f6dae5176ed51e2d4d30
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-06-09 01:15:07 +0800

    Using region specific lookups

--- preferences/src/main/java/com/yugabyte/samples/trading/security/CustomerBasedUserDetailsService.java
+++ preferences/src/main/java/com/yugabyte/samples/trading/security/CustomerBasedUserDetailsService.java
@@ -1,8 +1,10 @@
 package com.yugabyte.samples.trading.security;
 
+import com.yugabyte.samples.trading.model.RegionType;
 import com.yugabyte.samples.trading.repository.CustomerRepository;
 import java.util.Collections;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,16 +16,17 @@ import org.springframework.stereotype.Component;
 public class CustomerBasedUserDetailsService implements UserDetailsService {
 
   private final CustomerRepository customers;
+  private final RegionType appRegion;
 
-
-  public CustomerBasedUserDetailsService(CustomerRepository customers) {
+  public CustomerBasedUserDetailsService(CustomerRepository customers, @Value("${app.region:AP}") RegionType appRegion) {
     this.customers = customers;
+    this.appRegion = appRegion;
   }
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     log.info("{}: Received request for loading user details", username);
-    var customer = customers.getByEmail(username)
+    var customer = customers.findByIdRegionAndEmail(appRegion, username)
       .orElseThrow(() -> new UsernameNotFoundException("User doest not exists"));
     log.info("{}: User found", username);
     return new User(customer.getEmail(), customer.getPassword(), customer.getEnabled(), false, false, false, Collections.emptyList());
