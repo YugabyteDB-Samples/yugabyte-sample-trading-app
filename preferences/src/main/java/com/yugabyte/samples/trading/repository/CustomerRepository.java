42751ed836af623a7bfeedbfb2fafe01423c71ad
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-06-09 01:14:51 +0800

    Added region specific lookups

--- preferences/src/main/java/com/yugabyte/samples/trading/repository/CustomerRepository.java
+++ preferences/src/main/java/com/yugabyte/samples/trading/repository/CustomerRepository.java
@@ -2,9 +2,11 @@ package com.yugabyte.samples.trading.repository;
 
 import com.yugabyte.samples.trading.model.Customer;
 import com.yugabyte.samples.trading.model.CustomerPK;
+import com.yugabyte.samples.trading.model.RegionType;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import java.util.Optional;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -16,9 +18,11 @@ public interface CustomerRepository extends JpaRepository<Customer, CustomerPK> 
 
   Boolean existsByEmail(String email);
 
-  Optional<Customer> getByEmail(String email);
+  Boolean existsByIdRegionAndEmail( RegionType id_region, String email);
 
-  Boolean getCustomerByEmailAndPassword(String email, String encryptedPassword);
+  Optional<Customer> findByEmail(String email);
+
+  Optional<Customer> findByIdRegionAndEmail(RegionType id_region, String email);
 
   @Query(value = "SELECT NEXTVAL('customers_customer_id_seq')", nativeQuery = true)
   Integer nextAccountNumber();
