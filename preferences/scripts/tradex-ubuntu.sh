9b926e938db1f69be1d60f7e97136a8e08f24666
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2022-06-09 09:30:00 +0800

    Adding support to update script and jar

--- preferences/scripts/tradex-ubuntu.sh
+++ preferences/scripts/tradex-ubuntu.sh
@@ -9,6 +9,7 @@ SCRIPT="${BASH_SOURCE[0]}"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_DIR=$( cd ${SCRIPT_DIR} ; pwd)
 JAR_URL=${JAR_URL:="https://github.com/yugabyte/yugabyte-sample-trading-app/releases/download/latest/tradex-0.0.1-SNAPSHOT.jar"}
+SCRIPT_URL=${JAR_URL:="https://github.com/yugabyte/yugabyte-sample-trading-app/releases/download/latest/tradex-ubuntu.sh"}
 SPRING_ACTIVE_PROFILES=${SPRING_ACTIVE_PROFILES:=ysql,prod,$APP_REGION}
 WORK_DIR=${WORK_DIR:=.}
 JAVA_OPTS=${JAVA_OPTS:=}
@@ -18,9 +19,13 @@ PID_FILE=${WORK_DIR}/${APP_NAME}.pid
 function setup(){
   sudo apt update
   sudo apt upgrade -qqy
-  sudo apt install openjdk-17-jdk openjdk-17-jre -qqy
-  # Get this from the github project page https://github.com/yugabyte/yugabyte-sample-trading-app/releases/latest
+  sudo apt install openjdk-17-jre -qqy
   wget -O tradex.jar $JAR_URL
+  wget -O $SCRIPT $SCRIPT_URL
+}
+function fetch-updates(){
+  wget -O tradex.jar $JAR_URL
+  wget -O $SCRIPT https://github.com/yugabyte/yugabyte-sample-trading-app/releases/download/latest/tradex-ubuntu.sh
 }
 function app-run(){
   INITIAL_YSQL_HOST=${INITIAL_YSQL_HOST:?"INITIAL_YSQL_HOST not set"}
