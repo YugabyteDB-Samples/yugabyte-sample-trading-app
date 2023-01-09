1f5e561975a035b4d296c98a2f129f82c3ac1bf9
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-01-10 00:27:07 +0800

    Update output to include database dns

--- automation/output.tf
+++ automation/output.tf
@@ -2,21 +2,60 @@
 
 output "access-info" {
   value = <<EOF
-  ACCESS INFORMATION
-  ==================
+ACCESS INFORMATION
+==================
 
-  %{for location, ip in local.vm-ips}
-  Location: ${location}
-  ---------------------    
-    SSH Access:    
-      ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i ${local_file.ssh-key-private.filename} ubuntu@${ip}
-      ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i ${local_file.ssh-key-private.filename} ubuntu@${aws_route53_record.fqdn[location].name}
-    Web Access:
-      http://${aws_route53_record.fqdn[location].name}
-      https://${aws_route53_record.fqdn[location].name}
-      http://${aws_route53_record.fqdn[location].name}:8080
-      https://${aws_route53_record.fqdn[location].name}:8443
+%{for location, ip in local.vm-ips~}
+Location: ${location}
+---------------------
+  SSH Access:    
+    ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i ${local_file.ssh-key-private.filename} ubuntu@${ip}
+    ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i ${local_file.ssh-key-private.filename} ubuntu@${aws_route53_record.regional-app[location].name}
+  Web Access:
+    http://${aws_route53_record.regional-app[location].name}
+    https://${aws_route53_record.regional-app[location].name}
+    http://${aws_route53_record.regional-app[location].name}:8080
+    https://${aws_route53_record.regional-app[location].name}:8443
 
-  %{endfor}  
+%{endfor~}
+
+Database DNS:
+-------------
+
+  Single Region Multi Zone
+    %{for db-dns in aws_route53_record.single-region[*]~}
+    ${db-dns.name} -> ${join(",", db-dns.records)}
+    %{endfor~}
+
+    %{for db-dns in aws_route53_record.single-region-nodes[*]~}
+    ${db-dns.name} -> ${join(",", db-dns.records)}
+    %{endfor~}
+
+  Multi Region
+    %{for db-dns in aws_route53_record.multi-region[*]~}
+    ${db-dns.name} -> ${join(",", db-dns.records)}
+    %{endfor~}
+
+    %{for db-dns in aws_route53_record.multi-region-nodes[*]~}
+    ${db-dns.name} -> ${join(",", db-dns.records)}
+    %{endfor~}
+
+  Multi Region Read Replicas
+    %{for db-dns in aws_route53_record.multi-region-read-replica[*]~}
+    ${db-dns.name} -> ${join(",", db-dns.records)}
+    %{endfor~}
+
+    %{for db-dns in aws_route53_record.multi-region-read-replica-nodes[*]~}
+    ${db-dns.name} -> ${join(",", db-dns.records)}
+    %{endfor~}
+
+  Geo Partitioned
+    %{for db-dns in aws_route53_record.geopart[*]~}
+    ${db-dns.name} -> ${join(",", db-dns.records)}
+    %{endfor~}
+
+    %{for db-dns in aws_route53_record.geopart-nodes[*]~}
+    ${db-dns.name} -> ${join(",", db-dns.records)}
+    %{endfor~}
 EOF
 }
