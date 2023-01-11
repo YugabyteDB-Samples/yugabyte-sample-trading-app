4029ce48de5b13d82f50d480c737d30cebd47607
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-01-11 19:41:05 +0800

    output tserver topology and ips

--- automation/yba-universe-info/output.tf
+++ automation/yba-universe-info/output.tf
@@ -2,8 +2,18 @@ output "universe"{
   value = local.universe
 }
 output "primary-tservers" {
-  value = sort(local.pirmary-tservers)
+  value = local.primary-tservers
+}
+output "primary-tservers-ips" {
+  value = local.primary-tservers-ips
 }
 output "read-replica-tservers"{
-  value = sort(local.read-replica-tservers)
+  value = local.read-replica-tservers
+}
+
+output "read-replica-tservers-ips"{
+  value = local.read-replica-tservers-ips
+}
+output "nodes-topology-map" {
+  value = local.node-topology-map
 }
