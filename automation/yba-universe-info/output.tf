59a3fae82cb3ca69f0f5be2bb8ca28f639658907
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-02-01 13:57:28 +0800

    debug output

--- automation/yba-universe-info/output.tf
+++ automation/yba-universe-info/output.tf
@@ -17,3 +17,46 @@ output "read-replica-tservers-ips"{
 output "nodes-topology-map" {
   value = local.node-topology-map
 }
+
+
+output "debug" {
+  value = <<-DEBUG
+  
+  universe
+  ${jsonencode(local.universe)}
+  -----------------------------
+  details
+  ${jsonencode(local.details)}
+  -----------------------------
+  clusters
+  ${jsonencode(local.clusters)}
+  -----------------------------
+  nodeDetailsSet
+  ${jsonencode(local.nodeDetailsSet)}
+  -----------------------------
+  
+  primary-cluster-uuid
+  ${jsonencode(local.primary-cluster-uuid)}
+  -----------------------------
+  primary-tservers
+  ${jsonencode(local.primary-tservers)}
+  -----------------------------
+  primary-tservers-ips
+  ${jsonencode(local.primary-tservers-ips)}
+-----------------------------
+  
+  read-replicas-uuids
+  ${jsonencode(local.read-replicas-uuids)}
+  -----------------------------
+  read-replica-tservers
+  ${jsonencode(local.read-replica-tservers)}
+  -----------------------------
+  read-replica-tservers-ips
+  ${jsonencode(local.read-replica-tservers-ips)}
+  -----------------------------
+  
+  node-topology-map 
+  ${jsonencode(local.node-topology-map )}
+-----------------------------
+  DEBUG
+}
